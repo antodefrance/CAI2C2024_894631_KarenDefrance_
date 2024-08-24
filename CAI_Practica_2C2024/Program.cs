b2f8f788aad4311b82793f18fbf569e8edc9b9a2
antodefrance <antonella.defrance@gmail.com>
2024-08-24 14:55:42 -0300

    Inicio de Proyecto
    Primer set de cambios correspondientes a inicio de proyecto

---
+++ CAI_Practica_2C2024/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Practica_2C2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
